b93ab307b50fa2352516cd00bdc2dd907097ec63
ankitashrm215 <ankitashrm215@gmail.com>
2024-09-06 16:54:41 -0500

    added build output directory

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build' // should match the output_location in your workflow
+  }
 })
